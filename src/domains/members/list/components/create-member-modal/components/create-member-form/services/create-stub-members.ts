787370e49bf203ad099b99c5e41d3a2391b5263e
Andrew Husiev <andrew.husiev@gmail.com>
2025-08-11 18:41:00 -0700

    WIP

--- src/domains/members/list/components/create-member-modal/components/create-member-form/services/create-stub-members.ts
+++ src/domains/members/list/components/create-member-modal/components/create-member-form/services/create-stub-members.ts
@@ -1,6 +1,6 @@
 import { db } from '@/shared/db/db';
+import staff from '@/shared/temp/data/dump-staff-members.json';
 import type { Staff } from '@/shared/types/staff';
-import staff from '../data/dump-staff-members.json';
 
 export async function createStubMembers() {
   const newStaff = (staff as Staff[]).map((item) => ({ ...item, id: crypto.randomUUID() }));
