b758dcf8c17a809b35b03447291070c3125779e1
Andrew Husiev <andrew.husiev@gmail.com>
2025-08-21 21:17:28 -0700

    WIP

--- src/app/layout/AuthLayout.tsx
+++ src/app/layout/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 function AuthLayout() {
   return (
-    <div className="min-h-screen bg-[url(/login-page-bg.webp)] bg-cover bg-center flex items-center justify-center">
+    <div className="min-h-[100dvh] bg-[url(/login-page-bg.webp)] bg-cover bg-center flex items-center justify-center">
       <Outlet />
     </div>
   );
